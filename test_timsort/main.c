15db59e815656d527906194d96536480df719355
Jeremy <jeremy90307@gmail.com>
2024-06-28 04:20:06 +0800

    Add Timsort but do not integrate it yet
    Prepare to integrate Timsort into the preemptive
    coroutine for future work.

---
+++ test_timsort/main.c
@@ -0,0 +1,366 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+#include "sort_impl.h"
+
+typedef struct {
+    struct list_head list;
+    int val;
+    int seq;
+} element_t;
+
+#define SAMPLES 1000
+
+static void create_sample(struct list_head *head, element_t *space, int samples)
+{
+    printf("Creating sample\n");
+    for (int i = 0; i < samples; i++) {
+        element_t *elem = space + i;
+        elem->val = rand();
+        list_add_tail(&elem->list, head);
+    }
+}
+
+static void copy_list(struct list_head *from,
+                      struct list_head *to,
+                      element_t *space)
+{
+    if (list_empty(from))
+        return;
+
+    element_t *entry;
+    list_for_each_entry (entry, from, list) {
+        element_t *copy = space++;
+        copy->val = entry->val;
+        copy->seq = entry->seq;
+        list_add_tail(&copy->list, to);
+    }
+}
+
+int compare(void *priv, const struct list_head *a, const struct list_head *b)
+{
+    if (a == b)
+        return 0;
+
+    int res = list_entry(a, element_t, list)->val -
+              list_entry(b, element_t, list)->val;
+
+    if (priv)
+        *((int *) priv) += 1;
+
+    return res;
+}
+
+bool check_list(struct list_head *head, int count)
+{
+    if (list_empty(head))
+        return 0 == count;
+
+    element_t *entry, *safe;
+    size_t ctr = 0;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        ctr++;
+    }
+    int unstable = 0;
+    list_for_each_entry_safe (entry, safe, head, list) {
+        if (entry->list.next != head) {
+            if (entry->val > safe->val) {
+                fprintf(stderr, "\nERROR: Wrong order\n");
+                return false;
+            }
+            if (entry->val == safe->val && entry->seq > safe->seq)
+                unstable++;
+        }
+    }
+    if (unstable) {
+        fprintf(stderr, "\nERROR: unstable %d\n", unstable);
+        return false;
+    }
+
+    if (ctr != SAMPLES) {
+        fprintf(stderr, "\nERROR: Inconsistent number of elements: %ld\n", ctr);
+        return false;
+    }
+    return true;
+}
+
+typedef void (*test_func_t)(void *priv,
+                            struct list_head *head,
+                            list_cmp_func_t cmp);
+
+typedef struct {
+    char *name;
+    test_func_t impl;
+} test_t;
+
+/* Implementation of timsort */
+
+static inline size_t run_size(struct list_head *head)
+{
+    if (!head)
+        return 0;
+    if (!head->next)
+        return 1;
+    return (size_t) (head->next->prev);
+}
+
+struct pair {
+    struct list_head *head, *next;
+};
+
+static size_t stk_size;
+
+static struct list_head *merge(void *priv,
+                               list_cmp_func_t cmp,
+                               struct list_head *a,
+                               struct list_head *b)
+{
+    struct list_head *head;
+    struct list_head **tail = &head;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (cmp(priv, a, b) <= 0) {
+            *tail = a;
+            tail = &a->next;
+            a = a->next;
+            if (!a) {
+                *tail = b;
+                break;
+            }
+        } else {
+            *tail = b;
+            tail = &b->next;
+            b = b->next;
+            if (!b) {
+                *tail = a;
+                break;
+            }
+        }
+    }
+    return head;
+}
+
+static void build_prev_link(struct list_head *head,
+                            struct list_head *tail,
+                            struct list_head *list)
+{
+    tail->next = list;
+    do {
+        list->prev = tail;
+        tail = list;
+        list = list->next;
+    } while (list);
+
+    /* The final links to make a circular doubly-linked list */
+    tail->next = head;
+    head->prev = tail;
+}
+
+static void merge_final(void *priv,
+                        list_cmp_func_t cmp,
+                        struct list_head *head,
+                        struct list_head *a,
+                        struct list_head *b)
+{
+    struct list_head *tail = head;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (cmp(priv, a, b) <= 0) {
+            tail->next = a;
+            a->prev = tail;
+            tail = a;
+            a = a->next;
+            if (!a)
+                break;
+        } else {
+            tail->next = b;
+            b->prev = tail;
+            tail = b;
+            b = b->next;
+            if (!b) {
+                b = a;
+                break;
+            }
+        }
+    }
+
+    /* Finish linking remainder of list b on to tail */
+    build_prev_link(head, tail, b);
+}
+
+static struct pair find_run(void *priv,
+                            struct list_head *list,
+                            list_cmp_func_t cmp)
+{
+    size_t len = 1;
+    struct list_head *next = list->next, *head = list;
+    struct pair result;
+
+    if (!next) {
+        result.head = head, result.next = next;
+        return result;
+    }
+
+    if (cmp(priv, list, next) > 0) {
+        /* decending run, also reverse the list */
+        struct list_head *prev = NULL;
+        do {
+            len++;
+            list->next = prev;
+            prev = list;
+            list = next;
+            next = list->next;
+            head = list;
+        } while (next && cmp(priv, list, next) > 0);
+        list->next = prev;
+    } else {
+        do {
+            len++;
+            list = next;
+            next = list->next;
+        } while (next && cmp(priv, list, next) <= 0);
+        list->next = NULL;
+    }
+    head->prev = NULL;
+    head->next->prev = (struct list_head *) len;
+    result.head = head, result.next = next;
+    return result;
+}
+
+static struct list_head *merge_at(void *priv,
+                                  list_cmp_func_t cmp,
+                                  struct list_head *at)
+{
+    size_t len = run_size(at) + run_size(at->prev);
+    struct list_head *prev = at->prev->prev;
+    struct list_head *list = merge(priv, cmp, at->prev, at);
+    list->prev = prev;
+    list->next->prev = (struct list_head *) len;
+    --stk_size;
+    return list;
+}
+
+static struct list_head *merge_force_collapse(void *priv,
+                                              list_cmp_func_t cmp,
+                                              struct list_head *tp)
+{
+    while (stk_size >= 3) {
+        if (run_size(tp->prev->prev) < run_size(tp)) {
+            tp->prev = merge_at(priv, cmp, tp->prev);
+        } else {
+            tp = merge_at(priv, cmp, tp);
+        }
+    }
+    return tp;
+}
+
+static struct list_head *merge_collapse(void *priv,
+                                        list_cmp_func_t cmp,
+                                        struct list_head *tp)
+{
+    int n;
+    while ((n = stk_size) >= 2) {
+        if ((n >= 3 &&
+             run_size(tp->prev->prev) <= run_size(tp->prev) + run_size(tp)) ||
+            (n >= 4 && run_size(tp->prev->prev->prev) <=
+                           run_size(tp->prev->prev) + run_size(tp->prev))) {
+            if (run_size(tp->prev->prev) < run_size(tp)) {
+                tp->prev = merge_at(priv, cmp, tp->prev);
+            } else {
+                tp = merge_at(priv, cmp, tp);
+            }
+        } else if (run_size(tp->prev) <= run_size(tp)) {
+            tp = merge_at(priv, cmp, tp);
+        } else {
+            break;
+        }
+    }
+
+    return tp;
+}
+
+void timsort(void *priv, struct list_head *head, list_cmp_func_t cmp)
+{
+    stk_size = 0;
+
+    struct list_head *list = head->next, *tp = NULL;
+    if (head == head->prev)
+        return;
+
+    /* Convert to a null-terminated singly-linked list. */
+    head->prev->next = NULL;
+
+    do {
+        /* Find next run */
+        struct pair result = find_run(priv, list, cmp);
+        result.head->prev = tp;
+        tp = result.head;
+        list = result.next;
+        stk_size++;
+        tp = merge_collapse(priv, cmp, tp);
+    } while (list);
+
+    /* End of input; merge together all the runs. */
+    tp = merge_force_collapse(priv, cmp, tp);
+
+    /* The final merge; rebuild prev links */
+    struct list_head *stk0 = tp, *stk1 = stk0->prev;
+    while (stk1 && stk1->prev)
+        stk0 = stk0->prev, stk1 = stk1->prev;
+    if (stk_size <= 1) {
+        build_prev_link(head, head, stk0);
+        return;
+    }
+    merge_final(priv, cmp, head, stk1, stk0);
+}
+
+/* End of timsort implementation*/
+
+int main(void)
+{
+    struct list_head sample_head, warmdata_head, testdata_head;
+    int count;
+    int nums = SAMPLES;
+
+    /* Assume ASLR */
+    srand((uintptr_t) &main);
+
+    test_t tests[] = {
+        {.name = "timesort", .impl = timsort},
+        {NULL, NULL},
+    };
+    test_t *test = tests;
+
+    INIT_LIST_HEAD(&sample_head);
+
+    element_t *samples = malloc(sizeof(*samples) * SAMPLES);
+    element_t *warmdata = malloc(sizeof(*warmdata) * SAMPLES);
+    element_t *testdata = malloc(sizeof(*testdata) * SAMPLES);
+
+    create_sample(&sample_head, samples, nums);
+
+    while (test->impl) {
+        printf("==== Testing %s ====\n", test->name);
+        /* Warm up */
+        INIT_LIST_HEAD(&warmdata_head);
+        INIT_LIST_HEAD(&testdata_head);
+        copy_list(&sample_head, &testdata_head, testdata);
+        copy_list(&sample_head, &warmdata_head, warmdata);
+        test->impl(&count, &warmdata_head, compare);
+
+        /* Test */
+        count = 0;
+        test->impl(&count, &testdata_head, compare);
+        printf("  Comparisons:    %d\n", count);
+        printf("  List is %s\n",
+               check_list(&testdata_head, nums) ? "sorted" : "not sorted");
+        test++;
+    }
+
+    return 0;
+}
